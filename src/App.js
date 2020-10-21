6b3eafce7ed65981cc2f48735d9233f12fb0b221
William (Billy) Noyes <wbillypn@gmail.com>
2020-10-21 20:53:33 +0100

    imported all svg icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import NavItem from './NavItem';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
 function App() {
   return (
     <div className="app">
-      <h1>facebook dropdown</h1>
+      <Navbar>
+        <NavItem icon='🚀' />
+        <NavItem icon='🚀' />
+        <NavItem icon='🚀' />
+      </Navbar>
     </div>
   );
 }
